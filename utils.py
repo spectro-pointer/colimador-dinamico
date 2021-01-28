9727637a7901c2fefa7a9fd13943863bd5d48204
Federico Gutiérrez <fede.gutierrezm@gmail.com>
2021-01-28 19:42:25 -0300

    SQL UPDATE statement one code line.
    Se modifica función set_sp_config de forma que por cada valor solicitado por el usuario se agregue en un string que finalmente forme la sentencia de SQL para actualizar dichos valores en la base de datos logrando hacer dicha acción una sola vez al final del código.
    String final deseada:
    "UPDATE sp_config SET USE_RASPBERRY=?,CORRECT_VERTICAL_CAMERA=?,CORRECT_HORIZONTAL_CAMERA=?,CENTER_RADIUS=?,SHOW_CENTER_CIRCLE=?,ENABLE_PHOTO=?,ENABLE_VIDEO=?,RECORD_SECONDS=?"

--- utils.py
+++ utils.py
@@ -20,24 +20,36 @@ def delete_db(app):
 def load_db(app):
     
     spectro_pointer_config = {}
+    # Create empty list for appending every value
+    config_data = list()
+
     spectro_pointer_config['use_raspberry']             = int(USE_RASPBERRY)
+    config_data.append(spectro_pointer_config['use_raspberry'])
+
     spectro_pointer_config['correct_vertical_camera']   = int(CORRECT_VERTICAL_CAMERA)
-    spectro_pointer_config['correct_horizontal_camera'] = int(CORRECT_HORIZONTAL_CAMERA)
-    spectro_pointer_config['center_radius']             = int(CENTER_RADIUS)
-    spectro_pointer_config['show_center_circle']        = int(SHOW_CENTER_CIRCLE)
-    spectro_pointer_config['enable_photo']              = int(ENABLE_PHOTO)
-    spectro_pointer_config['enable_video']              = int(ENABLE_VIDEO)
-    spectro_pointer_config['record_seconds']            = int(RECORD_SECONDS)
+    config_data.append(spectro_pointer_config['correct_vertical_camera'])
     
-    # Create tuple with the config data
-    config_data = ( spectro_pointer_config['use_raspberry'],
-                    spectro_pointer_config['correct_vertical_camera'],
-                    spectro_pointer_config['correct_horizontal_camera'],
-                    spectro_pointer_config['center_radius'],
-                    spectro_pointer_config['show_center_circle'],
-                    spectro_pointer_config['enable_photo'],
-                    spectro_pointer_config['enable_video'],
-                    spectro_pointer_config['record_seconds'])
+    spectro_pointer_config['correct_horizontal_camera'] = int(CORRECT_HORIZONTAL_CAMERA)
+    config_data.append(spectro_pointer_config['correct_horizontal_camera'])
+    
+    spectro_pointer_config['center_radius']             = int(CENTER_RADIUS)
+    config_data.append(spectro_pointer_config['center_radius'])
+    
+    spectro_pointer_config['show_center_circle']        = int(SHOW_CENTER_CIRCLE)
+    config_data.append(spectro_pointer_config['show_center_circle'])
+    
+    spectro_pointer_config['enable_photo']              = int(ENABLE_PHOTO)
+    config_data.append(spectro_pointer_config['enable_photo'])
+    
+    spectro_pointer_config['enable_video']              = int(ENABLE_VIDEO)
+    config_data.append(spectro_pointer_config['enable_video'])
+    
+    spectro_pointer_config['record_seconds']            = int(RECORD_SECONDS)
+    config_data.append(spectro_pointer_config['record_seconds'])
+    
+    #  Transform config_data list into a tuple
+    config_data = tuple(config_data)
+
     # Insert tuple with config data into database
     with app.app_context():
         conn = connect_db()
@@ -71,30 +83,86 @@ def get_db():
 def set_sp_config(app,**spectro_pointer_config):
     with app.app_context():
         conn = connect_db()
-
-        if spectro_pointer_config['use_raspberry']:
-            conn.execute("UPDATE sp_config SET USE_RASPBERRY=?",(int(spectro_pointer_config['use_raspberry']),))
+        # Aux variable for value control
+        contVal = 0
+        # Create string were SQL statements will be added
+        s = "UPDATE sp_config SET "
+        # Create empty list were values to update will be appended
+        l = []
+        
+        # Check every possible value, if true, append value and extend SQL statement
+        if spectro_pointer_config['use_raspberry']:            
+            s = s + "USE_RASPBERRY=?"
+            l.append(int(spectro_pointer_config['use_raspberry']))
+            contVal+=1
 
         if spectro_pointer_config['correct_vertical_camera']:
-            conn.execute("UPDATE sp_config SET CORRECT_VERTICAL_CAMERA=?",(int(spectro_pointer_config['correct_vertical_camera']),))
+            s2 = "CORRECT_VERTICAL_CAMERA=?"
+            if contVal == 0:
+                s = s + s2
+            else:
+                s = s + "," + s2         
+            l.append(int(spectro_pointer_config['correct_vertical_camera']))
+            contVal+=1
 
         if spectro_pointer_config['correct_horizontal_camera']:
-            conn.execute("UPDATE sp_config SET CORRECT_HORIZONTAL_CAMERA=?",(int(spectro_pointer_config['correct_horizontal_camera']),))
+            s3 = "CORRECT_HORIZONTAL_CAMERA=?"
+            if contVal == 0:
+                s = s + s3
+            else:
+                s = s + "," + s3
+            l.append(int(spectro_pointer_config['correct_horizontal_camera']))
+            contVal+=1
 
         if spectro_pointer_config['center_radius']:
-            conn.execute("UPDATE sp_config SET CENTER_RADIUS=?",(int(spectro_pointer_config['center_radius']),))
+            s4 = "CENTER_RADIUS=?"
+            if contVal == 0:
+                s = s + s4
+            else:
+                s = s + "," + s4
+            l.append(int(spectro_pointer_config['center_radius']))
+            contVal+=1
 
         if spectro_pointer_config['show_center_circle']:
-            conn.execute("UPDATE sp_config SET SHOW_CENTER_CIRCLE=?",(int(spectro_pointer_config['show_center_circle']),))
+            s5 = "SHOW_CENTER_CIRCLE=?"
+            if contVal == 0:
+                s = s + s5
+            else:
+                s = s + "," + s5
+            l.append(int(spectro_pointer_config['show_center_circle']))
+            contVal+=1
 
         if spectro_pointer_config['enable_photo']:
-            conn.execute("UPDATE sp_config SET ENABLE_PHOTO=?",(int(spectro_pointer_config['enable_photo']),))
+            s6 = "ENABLE_PHOTO=?"
+            if contVal == 0:
+                s = s + s6
+            else:
+                s = s + "," + s6
+            l.append(int(spectro_pointer_config['enable_photo']))
+            contVal+=1
 
         if spectro_pointer_config['enable_video']:
-            conn.execute("UPDATE sp_config SET ENABLE_VIDEO=?",(int(spectro_pointer_config['enable_video']),))
-
+            s7 = "ENABLE_VIDEO=?"
+            if contVal == 0:
+                s = s + s7
+            else:
+                s = s + "," + s7
+            l.append(int(spectro_pointer_config['enable_video']))
+            contVal+=1
+            
         if spectro_pointer_config['record_seconds']:
-            conn.execute("UPDATE sp_config SET RECORD_SECONDS=?",(int(spectro_pointer_config['record_seconds']),))
+            s8 = "RECORD_SECONDS=?"
+            if contVal == 0:
+                s = s + s8
+            else:
+                s = s + "," + s8
+            l.append(int(spectro_pointer_config['record_seconds']))
+            contVal+=1
+        # Convert list into tuple
+        l = tuple(l)
+        
+        # Execute UPDATE statement
+        conn.execute('"'+s+'"',l)
 
         conn.commit()
         conn.close()
