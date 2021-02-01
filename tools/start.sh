a59665586912c4fc8357bf47a45b6e4e57d74010
Nicolas Tomatis <nicolas.tomatis.f@gmail.com>
2021-02-01 20:52:42 -0300

    Add documentation

--- tools/start.sh
+++ tools/start.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-python3 star_detector.py  --ip 0.0.0.0 --port 8081
+# Parse arguments
+while getopts "i:p:" opt;
+do
+    case $opt in
+        i) IP="$OPTARG"
+        ;;
+        p) PORT="$OPTARG"
+        ;;
+    esac
+done
+
+# Default arguments
+if [ -z  $IP ]; then
+    IP=0.0.0.0
+fi
+if [ -z  $PORT ]; then
+    PORT=8081
+fi
+python3 star_detector.py --ip $IP --port $PORT
