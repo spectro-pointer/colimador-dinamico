1c79138b5f15f8bec22105ef50a9bf9eab4e7d35
YohanHadji <hadji.yohan@gmail.com>
2023-12-30 17:09:29 +0100

    fix first seen

--- tracker_lib.py
+++ tracker_lib.py
@@ -547,10 +547,10 @@ def process_and_store_light_points(new_points):
             # Add new point to the list with acceleration and speed = 0 for both x and y
             name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
             # Make sure the name doesn't already exist in the current list:
-            while name in [existing_name for existing_name, _, _, _, _, _, _, _ in all_light_points]:
+            while name in [existing_name for existing_name, _, _, _, _, _, _, _, _ in all_light_points]:
                 name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
 
-            all_light_points.append((name,current_time, new_x, new_y, current_time, 0, 0, 0, 0))
+            all_light_points.append((name, current_time, new_x, new_y, current_time, 0, 0, 0, 0))
 
     all_light_points = [(name, firstSeen, x, y, timestamp, speed_x, speed_y, acceleration_x, acceleration_y) for name, firstSeen, x, y, timestamp, speed_x, speed_y, acceleration_x, acceleration_y in all_light_points if current_time - timestamp <= constrain(map_range(current_time-firstSeen,0,10,0.1,3),0.1,3)]
 
