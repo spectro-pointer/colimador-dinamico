03145e9ee100bd041c9e76e5578910c814e0f489
YohanHadji <hadji.yohan@gmail.com>
2023-12-31 19:16:30 +0100

    Test center

--- tracker_lib.py
+++ tracker_lib.py
@@ -901,7 +901,7 @@ def camera_loop(app):
                 deltaY = y-last_estimated_y
 
                 #draw this as a red point on frame2 but change the coordinate so that the center of the picture is (0,0)
-                cv2.circle(frame2, (int(x+SIZE[0]/2), int(y+SIZE[1]/2)), 5, (0, 0, 255), -1) # -1 hace  el relleno del circulo
+                cv2.circle(frame2, (int(SIZE[0]/2), int(SIZE[1]/2)), 5, (0, 0, 255), -1) # -1 hace  el relleno del circulo
 
                 last_estimated_x, last_estimated_y = estimate_position((x, y), speed_x, speed_y, acceleration_x, acceleration_y, oldTime, time.time())
                 break
